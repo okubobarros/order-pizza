a8c036532df771f7dc5312d47d28e2df4219bfbf
okubobarros <okubobarros@gmail.com>
2022-06-14 00:22:55 -0300

    css e lista pizza

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'', price: 20.19, sizes:['100g', '320g', '550g']},
+    {id:2, name:'Calabresa', img:'', price: 18.19, sizes:['100g', '320g', '550g']},
+    {id:3, name:'Quatro Queijos', img:'', price: 17.19, sizes:['100g', '320g', '550g']},
+    {id:4, name:'Americana', img:'', price: 19.19, sizes:['100g', '320g', '550g']},
+    {id:5, name:'Sorvete', img:'', price: 21.19, sizes:['100g', '320g', '550g']},
+    {id:6, name:'Moda da Casa', img:'', price: 16.19, sizes:['100g', '320g', '550g']},
+    {id:7, name:'Chocolate', img:'', price: 22.19, sizes:['100g', '320g', '550g']},
+];
