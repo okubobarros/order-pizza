0530f86f34b9f5c934f0fca15bde6a4b7b408e1e
okubobarros <okubobarros@gmail.com>
2022-06-14 02:00:35 -0300

    css update

--- pizzas.js
+++ pizzas.js
@@ -1,9 +1,9 @@
 let pizzaJson = [
-    {id:1, name:'Mussarela', img:'', price: 20.19, sizes:['100g', '320g', '550g']},
-    {id:2, name:'Calabresa', img:'', price: 18.19, sizes:['100g', '320g', '550g']},
-    {id:3, name:'Quatro Queijos', img:'', price: 17.19, sizes:['100g', '320g', '550g']},
-    {id:4, name:'Americana', img:'', price: 19.19, sizes:['100g', '320g', '550g']},
-    {id:5, name:'Sorvete', img:'', price: 21.19, sizes:['100g', '320g', '550g']},
-    {id:6, name:'Moda da Casa', img:'', price: 16.19, sizes:['100g', '320g', '550g']},
-    {id:7, name:'Chocolate', img:'', price: 22.19, sizes:['100g', '320g', '550g']},
+    {id:1, name:'Mussarela', img:'images/pizza.jpg', price: 20.19, sizes:['100g', '320g', '550g'], description: 'Pizza mussarela - em mais de uma linha fica mais interessante' },
+    {id:2, name:'Calabresa', img:'images/pizza2.jpg', price: 18.19, sizes:['100g', '320g', '550g'], description: 'Pizza calabresa - em mais de uma linha fica mais interessante'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.jpg', price: 17.19, sizes:['100g', '320g', '550g'], description: 'Pizza quatro queijos - em mais de uma linha fica mais interessante'},
+    {id:4, name:'Americana', img:'images/pizza4.jpg', price: 19.19, sizes:['100g', '320g', '550g'], description:'Pizza americana - em mais de uma linha fica mais interessante'},
+    {id:5, name:'Sorvete', img:'images/pizza5.jpg', price: 21.19, sizes:['100g', '320g', '550g'], description:'Pizza sorvete - em mais de uma linha fica mais interessante'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.jpg', price: 16.19, sizes:['100g', '320g', '550g'], description:'Pizza moda da casa - em mais de uma linha fica mais interessante'},
+    {id:7, name:'Chocolate', img:'images/pizza7.jpg', price: 22.00, sizes:['100g', '320g', '550g'], description:'Pizza chocolate - em mais de uma linha fica mais interessante'},
 ];
--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const cs = (el)=>document.querySelectorAll(el);
 
 pizzaJson.map((item, index)=> {
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
+
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     //preencher as informações em pizzaitem
     c('.pizza-area').append(pizzaItem);
 });
